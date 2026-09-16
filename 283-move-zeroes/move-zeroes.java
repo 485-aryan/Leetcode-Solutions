2a57fa9f4c6c34c9378ea32f192cc6d6007a0583
485-aryan <485.aryan@gmail.com>
2026-09-16 19:17:01 +0530

    Time: 2 ms (91.73%) | Memory: 47.6 MB (87.86%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int temp[] = new int[nums.length];
+        int count =0;
+        int j = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]==0){
+                count++;
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=0){
+                temp[j]=nums[i];
+                j++;
+            }
+        }
+        for(int i=(nums.length-count); i<nums.length; i++){
+            temp[i]=0;
+        }
+        for(int i=0; i<nums.length; i++){
+            nums[i]=temp[i];
+        }
+    }
+}
